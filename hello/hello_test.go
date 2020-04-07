bb99de40db64ffae47800dd072668419df3d9c4a
Muhammad Habibullah <mhabibullah14@yahoo.com>
2020-04-08 04:39:06 +0700

    fix: refactor main package in hello

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -1,4 +1,4 @@
-package main
+package hello
 
 import "testing"
 
@@ -14,28 +14,24 @@ func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Habib", "")
 		want := "Hello, Habib"
-
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("", "")
 		want := "Hello, World"
-
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("saying hello in Spanish", func(t *testing.T) {
 		got := Hello("Leo", spanish)
 		want := "Hola, Leo"
-
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("saying hello in French", func(t *testing.T) {
 		got := Hello("Matt", french)
 		want := "Bonjour, Matt"
-
 		assertCorrectMessage(t, got, want)
 	})
 }
